54b696c0e2f554bee17830af2f7e0e950ceb9b61
Влад <clearsky7free@gmail.com>
2013-10-24 20:54:29 +0300

    Initial Commit

---
+++ KNURE-Sked/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KNURE-Sked
+//
+//  Created by Влад on 10/24/13.
+//  Copyright (c) 2013 Влад. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
