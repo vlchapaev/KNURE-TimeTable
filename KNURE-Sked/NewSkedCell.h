91713ca6485c2ce492e7136ff28e5f435ff7f227
Влад <clearsky7free@gmail.com>
2013-12-01 12:15:40 +0200

    Добавлена новая вьюха с заполнением новой пары (не рабочая)

---
+++ KNURE-Sked/NewSkedCell.h
@@ -0,0 +1,13 @@
+//
+//  NewSkedCell.h
+//  KNURE-Sked
+//
+//  Created by Влад on 11/30/13.
+//  Copyright (c) 2013 Влад. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewSkedCell : UIViewController
+
+@end
---
+++ KNURE-Sked/NewSkedCell.m
@@ -0,0 +1,35 @@
+//
+//  NewSkedCell.m
+//  KNURE-Sked
+//
+//  Created by Влад on 11/30/13.
+//  Copyright (c) 2013 Влад. All rights reserved.
+//
+
+#import "NewSkedCell.h"
+
+@interface NewSkedCell ()
+
+@end
+
+@implementation NewSkedCell
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
