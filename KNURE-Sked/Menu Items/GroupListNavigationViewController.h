cfb6fd032ba532e9ae22988b4071e5cc4a6d685d
Влад <clearsky7free@gmail.com>
2014-01-09 20:56:38 +0200

    Какая-то херня, которая хз чего не закомитилась.

---
+++ KNURE-Sked/Menu Items/GroupListNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupListNavigationViewController.h
+//  KNURE-Sked
+//
+//  Created by Влад on 1/4/14.
+//  Copyright (c) 2014 Влад. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupListNavigationViewController : UINavigationController
+
+@end
