16c6e1516ca0e686b6231a5a2f9d9113f642de6f
Vladislav Chapaev <chapaev.vlad@gmail.com>
2019-02-04 10:11:42 +0300

    [layers] add empty assembly files

---
+++ KNURE TimeTable iOS/Presentation/PresentationLayerAssembly.h
@@ -0,0 +1,17 @@
+//
+//  PresentationLayerAssembly.h
+//  KNURE TimeTable iOS
+//
+//  Created by Vladislav Chapaev on 04/02/2019.
+//  Copyright © 2019 Vladislav Chapaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PresentationLayerAssembly : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
