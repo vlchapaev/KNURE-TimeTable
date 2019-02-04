649728952d697d2ce19418f1c32cd98a4eed9e92
Vladislav Chapaev <chapaev.vlad@gmail.com>
2019-02-04 10:09:23 +0300

    [layers] fix folders

---
+++ KNURE TimeTable iOS/Application/ApplicationLayerAssembly.h
@@ -0,0 +1,17 @@
+//
+//  ApplicationLayerAssembly.h
+//  KNURE TimeTable iOS
+//
+//  Created by Vladislav Chapaev on 04/02/2019.
+//  Copyright © 2019 Vladislav Chapaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ApplicationLayerAssembly : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
