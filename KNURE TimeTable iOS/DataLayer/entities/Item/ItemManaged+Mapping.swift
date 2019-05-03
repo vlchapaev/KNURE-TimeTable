e0b9b085efc0a1f15f00c84afa7172885df4d855
Vladislav Chapaev <chapaev.vlad@gmail.com>
2019-05-03 18:27:23 +0300

    [mapping] fix data model + mapping extensions

---
+++ KNURE TimeTable iOS/DataLayer/entities/Item/ItemManaged+Mapping.swift
@@ -0,0 +1,16 @@
+//
+//  ItemManaged+Mapping.swift
+//  KNURE TimeTable iOS
+//
+//  Created by Vladislav Chapaev on 03/05/2019.
+//  Copyright © 2019 Vladislav Chapaev. All rights reserved.
+//
+
+import Foundation
+
+extension ItemManaged {
+
+	static func defaultMapping() {
+	}
+
+}
---
+++ KNURE TimeTable iOS/DataLayer/entities/Lesson/LessonManaged+Mapping.swift
@@ -0,0 +1,14 @@
+//
+//  LessonManaged+Mapping.swift
+//  KNURE TimeTable iOS
+//
+//  Created by Vladislav Chapaev on 03/05/2019.
+//  Copyright © 2019 Vladislav Chapaev. All rights reserved.
+//
+
+import Foundation
+
+extension LessonManaged {
+	static func defaultMapping() {
+	}
+}
