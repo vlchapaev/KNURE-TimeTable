e2fce0c44713699ad9a123db9ef3c1a6e0f1fdf1
Vladislav Chapaev <chapaev.vlad@gmail.com>
2019-05-12 21:09:54 +0300

    [domainLayer] register usecases

--- KNURE TimeTable iOS/DomainLayer/DomainLayerAssembly.swift
+++ KNURE TimeTable iOS/DomainLayer/DomainLayerAssembly.swift
@@ -11,5 +11,27 @@ import Swinject
 
 class DomainLayerAssembly: Assembly {
 	func configure(_ container: Container) {
+		configureItem(container)
+		configureLesson(container)
+	}
+
+	func configureItem(_ container: Container) {
+		container.register(SaveItemUseCase.self) { resolver in
+			SaveItemUseCase(itemRepository: resolver.resolve(ItemRepository.self)!)
+		}
+
+		container.register(RemoveItemUseCase.self) { resolver in
+			RemoveItemUseCase(itemRepository: resolver.resolve(ItemRepository.self)!)
+		}
+
+		container.register(ItemsUseCase.self) { resolver in
+			ItemsUseCase(itemRepository: resolver.resolve(ItemRepository.self)!)
+		}
+	}
+
+	func configureLesson(_ container: Container) {
+		container.register(UpdateTimetableUseCase.self) { resolver in
+			UpdateTimetableUseCase(lessonRepository: resolver.resolve(LessonRepository.self)!)
+		}
 	}
 }
